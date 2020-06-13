70ebee33d3cffca4c0320c0a430524f7414fee51
Robert Wilcox <42777835+stampylongr@users.noreply.github.com>
2020-06-13 11:17:53 -0400

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+// define settings here.
+
+// database settings.
+const DB_HOST = 'localhost'; // hostname of the database. required.
+const DB_USER = 'root'; // database user. required.
+const DB_PASS = ''; // password for the database user. optional.
+const DB_NAME = 'koid_db'; // name of the database. required.
+
+// cloudinary settings.
+const CLOUD_NAME = 'insert-account-name'; // cloudinary account name. required.
+const CLOUD_PRESET = 'default'; // cloudinary upload preset. required.
+
+// recaptcha settings.
+const RE_PUB = null; // recaptcha public key. optional.
+const RE_SCRT = null; // recaptcha secret key. optional.
+
+// various.
+const CONTACT_EMAIL = ''; // the email you want to use for emergency contact (e.g. dmca notices etc.) optional.
